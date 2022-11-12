54bf34ed1d479ec3b6e277f3ee138e2027b39add
Пламена <plkoleva2005@gmail.com>
2022-11-12 13:30:03 +0200

    Solved problem

---
+++ src/ConditionalStatementsAdvanced/Weekend_or_Working_Day_02.java
@@ -0,0 +1,22 @@
+package ConditionalStatementsAdvanced;
+
+import java.util.Scanner;
+
+public class Weekend_or_Working_Day_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        switch(s){
+            case "Monday":
+            case "Tuesday":
+            case "Wednesday":
+            case "Thursday":
+            case "Friday":
+              System.out.println("Working day");break;
+            case "Saturday":
+            case "Sunday":
+                System.out.println("Weekend");break;
+            default : System.out.println("Error");break;
+        }
+    }
+}
