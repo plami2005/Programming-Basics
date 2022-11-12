7b3ccc06e309b912dad9d65a228256970884b4e9
Пламена <plkoleva2005@gmail.com>
2022-11-12 13:29:47 +0200

    Solved problem

---
+++ src/ConditionalStatementsAdvanced/Day_of_week_01.java
@@ -0,0 +1,21 @@
+package ConditionalStatementsAdvanced;
+
+import java.util.Scanner;
+
+public class Day_of_week_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+            switch (n){
+                case 1: System.out.println("Monday");break;
+                case 2: System.out.println("Tuesday");break;
+                case 3: System.out.println("Wednesday");break;
+                case 4: System.out.println("Thursday");break;
+                case 5: System.out.println("Friday");break;
+                case 6: System.out.println("Saturday");break;
+                case 7: System.out.println("Sunday");break;
+                default: System.out.println("Error");break;
+            }
+
+    }
+}
