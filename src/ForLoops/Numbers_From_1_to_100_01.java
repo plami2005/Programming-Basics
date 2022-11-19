ccc2a121e06b52fa0e7981ce7ecdb34ae052599e
Пламена <plkoleva2005@gmail.com>
2022-11-19 10:45:27 +0200

    Solved problem

---
+++ src/ForLoops/Numbers_From_1_to_100_01.java
@@ -0,0 +1,12 @@
+package ForLoops;
+
+import java.util.Scanner;
+
+public class Numbers_From_1_to_100_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i=1; i<=100; i++){
+            System.out.println(i);
+        }
+    }
+}
