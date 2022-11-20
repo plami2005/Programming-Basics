c4ba4f1f521f193ae6fa2e3004fde6e2208218b7
Пламена <plkoleva2005@gmail.com>
2022-11-20 11:33:04 +0200

    Solved problem

---
+++ src/ForLoopsExercise/Numbers_Ending_in_7_01.java
@@ -0,0 +1,9 @@
+package ForLoopsExercise;
+
+public class Numbers_Ending_in_7_01 {
+    public static void main(String[] args) {
+        for (int i=7;i<=997;i+=10){
+            System.out.println(i);
+        }
+    }
+}
